7d2f4f6a3ca1942b6febef1793ccbdc1fe949dde
mcflyhalf <mcflyhalf@live.com>
2021-04-28 12:26:02 +0400

    Clean up comments. Set up next branch of dev: biz logic

--- loko/app.py
+++ loko/app.py
@@ -18,14 +18,5 @@ def load_user(user_id):
     return session.query(models.Users).filter(models.Users.id==int(user_id)).one_or_none()
 
 
-raise
-#Next, implement business logic:
-# * Wallet creation
-# * Transfer from a wallet to another
-# * Wallet balance display (working with decimal points)
-# * Currency List
-# * Exchange rates (table?). Do implementation research, to implement tomorrow
-
-
 app.register_blueprint(index_blueprint)
 app.register_blueprint(auth_blueprint)
