96d6f266a8e8968983b54a7e3953fa1c14ee7b88
mcflyhalf <mcflyhalf@live.com>
2021-05-01 01:44:42 +0400

    Populate dev db during setup to give some testing data

---
+++ loko/tests/populate.py
@@ -0,0 +1,53 @@
+import os
+from loko.biz.creator import UserCreator
+from loko.biz.xchange import get_xchange_data, save_xchange_data
+from loko import get_configs
+from loko.models.models import Users, Wallets, SupportedCurrencies
+
+
+def populate_db(session, xchange_response_file):
+	'''
+	Populates db with dummy data to start off the application
+	'''
+	# Populate Currencies table
+	xchange_data = get_xchange_data(xchange_response_file)
+	save_xchange_data(xchange_data, session=session, save_to_db=True)
+	sup_cur_list = list(SupportedCurrencies)
+	tot_cur = len(sup_cur_list)
+
+	# Populate Users and Wallets table. Each user gets a corresponding wallet
+	for _id in range(7):
+		usr = {}
+		usr['id'] = _id
+		if _id == 0:
+			usr['username'] = 'lokoadmin'
+			usr['password'] = 'admin123'
+			usr['name'] = 'Loko Admin'
+			usr['email'] = 'admin@lo.ko'
+			usr['authenticated'] = False
+			usr['active'] = True
+			usr['default_currency_alpha_code'] = 'EUR'
+			wallet_balance = 0
+
+		else:
+			usr['username'] = 'dev'+str(_id)
+			usr['password'] = 'dev123'
+			usr['name'] = 'Develo Per'+str(_id)
+			usr['email'] = 'dev{}@dev.mail'.format(str(_id))
+			usr['authenticated'] = False
+			usr['active'] = True
+			alpha_code = sup_cur_list[(_id+4)%tot_cur].name
+			usr['default_currency_alpha_code'] = alpha_code
+			wallet_balance = 1000
+
+		usercreator = UserCreator(session, **usr)
+		user_and_wallet = usercreator.create_user()
+		wallet = user_and_wallet['wallet']
+		wallet.balance = wallet_balance
+		session.flush()
+
+	session.commit()
+
+
+
+
--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@ from setuptools import setup, find_packages
 from loko import LokoConfig, get_logger, models 
 from loko.conf import CONFIG_FILENAME
 from loko.biz.xchange import XCHANGE_FILENAME_BASE
+from loko.tests.populate import populate_db
 import configparser
 import os
 
 
 setup(
 	name = "loko",
-	version = "0.1.5",
+	version = "0.1.8",
 	author = "Mcflyhalf",
 	author_email = "mcflyhalf@live.com",
 	description = ("Store, send and receive money in different currencies"),
@@ -30,15 +31,23 @@ default['sqlite test db'] = os.path.join(config['DEFAULT']['Install Location'], 
 default['config_file'] = os.path.join(os.getcwd(), CONFIG_FILENAME)
 default['exchange rate base filename'] = XCHANGE_FILENAME_BASE
 default['exchange rate file location'] = os.path.join(config['DEFAULT']['Install Location'], 'loko','biz', 'xchange_data')
+default['exchange rate test response'] = os.path.join(config['DEFAULT']['Install Location'], 'loko','tests', 'test_xchange_resp.pkl')
 #Create config file
 with open(CONFIG_FILENAME, 'w') as configfile:
 	config.write(configfile)
 
 
 prod_engine = models.production_engine
-LokoConfig(prod_engine, logger).newconfig(prod_engine)
+LokoConfig(prod_engine, logger).newconfig(prod_engine,drop_tables=False)
 
 dev_engine = models.development_engine
-LokoConfig(dev_engine, logger).newconfig(dev_engine)
+LokoConfig(dev_engine, logger).newconfig(dev_engine,drop_tables=True)
 
-
+#Populate the dev db for dev testing
+#For some reason `pip install -e .` seems to run this entire file twice over
+logger.info("Preparing to populate dev database")
+xchange_resp_file = default['exchange rate test response']
+dev_session = models.get_db_session(env="development")
+logger.info("Starting to populate dev database")
+populate_db(dev_session, xchange_resp_file)
+logger.info("Finished populating dev database")
